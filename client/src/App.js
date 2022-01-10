a7ae8a17f041b6d1a4d99034f12ac9fa04465a2a
JunHoBaek <bemodesty306@naver.com>
2022-01-10 23:19:37 +0900

    react 구조 설계

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Main from "./routes/Main.js";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={Main}></Route>
+        <Route path="/login"></Route>
+        <Route path="/signup"></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
